eda81f7a61734e8d5e500d7944f80ea01a7b600a
jfitzsimmons <jfitzhome@gmail.com>
2019-08-24 16:23:23 -0500

    Updates

---
+++ precache-manifest.f40a198ab7d5a58b90b6765a2952c26f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb3732eda64182d0f8db06b8b37453b",
+    "url": "/Art-API-React/index.html"
+  },
+  {
+    "revision": "774abf86e4b7e5c25f75",
+    "url": "/Art-API-React/static/css/main.4b3e02e3.chunk.css"
+  },
+  {
+    "revision": "108006061624a3407edb",
+    "url": "/Art-API-React/static/js/2.e9b635fd.chunk.js"
+  },
+  {
+    "revision": "774abf86e4b7e5c25f75",
+    "url": "/Art-API-React/static/js/main.33957998.chunk.js"
+  },
+  {
+    "revision": "0d578e641eef61c87dce",
+    "url": "/Art-API-React/static/js/runtime~main.e3cab0cc.js"
+  }
+]);
