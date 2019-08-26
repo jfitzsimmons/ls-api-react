e6a4db36f3e398b4297d2d1ff4e27a7ce592e31c
jfitzsimmons <jfitzhome@gmail.com>
2019-08-26 11:35:10 -0500

    Updates

---
+++ precache-manifest.ebfc2fa7166b370ffb3a50c1d1c1fcf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2adeb0513372334fe271d967a0617d22",
+    "url": "/Art-API-React/index.html"
+  },
+  {
+    "revision": "ff2547e61ea12e3536d6",
+    "url": "/Art-API-React/static/css/main.b06cd6f5.chunk.css"
+  },
+  {
+    "revision": "eb9531b05e38475be753",
+    "url": "/Art-API-React/static/js/2.1b772760.chunk.js"
+  },
+  {
+    "revision": "ff2547e61ea12e3536d6",
+    "url": "/Art-API-React/static/js/main.b9b882ad.chunk.js"
+  },
+  {
+    "revision": "0d578e641eef61c87dce",
+    "url": "/Art-API-React/static/js/runtime~main.e3cab0cc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Art-API-React/precache-manifest.36b5e413ac9e190c01026f88f025dc30.js"
+  "/Art-API-React/precache-manifest.ebfc2fa7166b370ffb3a50c1d1c1fcf1.js"
 );
 
 self.addEventListener('message', (event) => {
