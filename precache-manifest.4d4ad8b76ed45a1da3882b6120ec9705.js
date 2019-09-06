a6cd24b294838b5a955ed1af1cee395c8ea01ea2
jfitzsimmons <jfitzhome@gmail.com>
2019-09-05 20:09:24 -0500

    Updates

--- precache-manifest.4d4ad8b76ed45a1da3882b6120ec9705.js
+++ precache-manifest.4d4ad8b76ed45a1da3882b6120ec9705.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3cba9d1306f6da001f9014334cecfb4b",
+    "revision": "4a84f060de650b623e4a9fb6cb30daf1",
     "url": "/Art-API-React/index.html"
   },
   {
-    "revision": "8f8ae78b03125d564fbc",
-    "url": "/Art-API-React/static/css/main.3ec6b09e.chunk.css"
+    "revision": "493bda9227a6e3b9fbe6",
+    "url": "/Art-API-React/static/css/main.df43b0d3.chunk.css"
   },
   {
     "revision": "eb9531b05e38475be753",
     "url": "/Art-API-React/static/js/2.1b772760.chunk.js"
   },
   {
-    "revision": "8f8ae78b03125d564fbc",
-    "url": "/Art-API-React/static/js/main.40391148.chunk.js"
+    "revision": "493bda9227a6e3b9fbe6",
+    "url": "/Art-API-React/static/js/main.fa989e17.chunk.js"
   },
   {
     "revision": "0d578e641eef61c87dce",
