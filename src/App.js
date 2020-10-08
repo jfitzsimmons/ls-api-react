ee470f2884f2d7f47fa74f5343f51b81f98af940
jfitzsimmons <jfitzhome@gmail.com>
2020-10-08 10:51:32 -0500

    airbnb es and entity changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React, {PureComponent} from 'react';
-import './App.scss';
-import {Search} from './Search.js';
-import {Painting} from './Painting.js';
+import React, { PureComponent } from "react";
+import "./App.scss";
+import { Search } from "./Search.js";
+import { Entity } from "./Entity.js";
 
 class App extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      title: "earth, wind, fire"
-    }
+      title: "earth, wind, fire",
+    };
     this.updateTitle = this.updateTitle.bind(this);
   }
 
   updateTitle(subject) {
-    this.setState({title: subject});
+    this.setState({
+      title: subject,
+    });
   }
 
   render() {
     return (
-      <div id = "App" className = "App" >
-        <Search update={this.updateTitle}/>
-        <Painting title = {this.state.title} />
+      <div id="App" className="App">
+        <Search update={this.updateTitle} /> <Entity title={this.state.title} />{" "}
       </div>
     );
   }
---
+++ src/Entity.js
@@ -0,0 +1,215 @@
+import React, { Component } from "react";
+import "./App.scss";
+import { Wiki } from "./Wiki.js";
+import { paginate } from "./Helpers.js";
+
+const ART_API_KEY = `${process.env.REACT_APP_ART_API_KEY}`;
+
+export class Entity extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: -1,
+      returnError: false,
+    };
+    this.records = {};
+    this.paginate = paginate.bind(this);
+    this.setCity = this.setCity.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchPaintingData();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.title !== prevProps.title) {
+      this.setState(
+        {
+          page: 0,
+        },
+        this.fetchPaintingData()
+      );
+    }
+  }
+
+  getCssValuePrefix() {
+    let rtrnVal = "";
+    const prefixes = ["-o-", "-ms-", "-moz-", "-webkit-"];
+    let dom = document.createElement("div");
+    for (var i = 0; i < prefixes.length; i++) {
+      dom.style.background = prefixes[i] + "linear-gradient(#000000, #ffffff)";
+      if (dom.style.background) rtrnVal = prefixes[i];
+    }
+    dom = null;
+    return this.setStyle(rtrnVal);
+  }
+
+  setStyle(prefix) {
+    const colors = this.records[this.state.page].colors;
+    let gradient = "";
+    for (let i = colors.length; i--; ) {
+      gradient += colors[i].color;
+      gradient += i === 0 ? ")" : ", ";
+    }
+    document.body.style.background = `radial-gradient(circle at bottom right, ${gradient}`;
+  }
+
+  objToQueryString(obj) {
+    const keyValuePairs = [];
+    for (const key in obj) {
+      keyValuePairs.push(
+        encodeURIComponent(key) + "=" + encodeURIComponent(obj[key])
+      );
+    }
+    return keyValuePairs.join("&");
+  }
+
+  queryString() {
+    return this.objToQueryString({
+      apikey: ART_API_KEY,
+      title: this.props.title,
+      classification: "Paintings",
+      hasimage: 1,
+      q: "imagepermissionlevel:0",
+      person: "any",
+      sort: "random",
+      color: "any",
+    });
+  }
+
+  fetchPaintingData() {
+    fetch(`https://api.harvardartmuseums.org/object?${this.queryString()}`, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.records = responseData.records;
+        this.records.length === 0
+          ? this.setState({
+              returnError: true,
+            })
+          : this.setState({
+              page: 0,
+            });
+      })
+      .catch((error) => {
+        this.setState({
+          returnError: true,
+        });
+        console.log(error);
+      });
+  }
+
+  setCity() {
+    const birthplace = this.records[this.state.page].people[0].birthplace;
+    if (birthplace) {
+      return birthplace.length > 23 ? birthplace.split(" ").pop() : birthplace;
+    } else if (this.records[this.state.page].culture) {
+      return this.records[this.state.page].culture;
+    } else {
+      const division = this.records[this.state.page].division;
+      return division.substr(0, division.indexOf(" "));
+    }
+  }
+
+  render() {
+    if (this.records[this.state.page]) {
+      this.getCssValuePrefix();
+      return (
+        <div>
+          <div className="render-coontainer">
+            <div className="painting flx-ctr">
+              <div className="painting__frame flx-ctr">
+                <span className="heading">
+                  {" "}
+                  {this.records[this.state.page].title}{" "}
+                </span>{" "}
+                <div className="frame__cell left">
+                  <img
+                    className="painting__image"
+                    src={this.records[this.state.page].primaryimageurl}
+                    alt={"image of " + this.records[this.state.page].title}
+                  />{" "}
+                </div>{" "}
+                <div className="frame__cell right">
+                  <div className="painting__label">
+                    <span className="label__title row">
+                      {" "}
+                      {this.records[this.state.page].title}{" "}
+                    </span>{" "}
+                    <span className="label__artist row">
+                      {" "}
+                      {this.records[this.state.page].people[0].name}{" "}
+                    </span>{" "}
+                    <span className="label__region row">
+                      {" "}
+                      {this.setCity()}{" "}
+                    </span>{" "}
+                    <span className="label__dated row">
+                      {" "}
+                      {this.records[this.state.page].dated}{" "}
+                    </span>{" "}
+                    <span className="label__period row">
+                      {" "}
+                      {this.records[this.state.page].period}{" "}
+                    </span>{" "}
+                    <span className="label__medium row">
+                      {" "}
+                      {this.records[this.state.page].medium}{" "}
+                    </span>{" "}
+                  </div>{" "}
+                  <div className="painting__paging page">
+                    {" "}
+                    {this.state.page + 1} of {this.records.length} <br />
+                    <button
+                      className="prev"
+                      onClick={() => this.paginate(-1)}
+                      disabled={this.state.page === 0}
+                    >
+                      {" "}
+                      previous{" "}
+                    </button>{" "}
+                    |{" "}
+                    <button
+                      className="next"
+                      onClick={() => this.paginate(1)}
+                      disabled={this.state.page === this.records.length - 1}
+                    >
+                      next
+                    </button>
+                  </div>{" "}
+                </div>{" "}
+              </div>{" "}
+            </div>{" "}
+          </div>{" "}
+          <Wiki city={this.setCity()} update={this.updateLatLng} />{" "}
+        </div>
+      );
+    } else {
+      let returnError = this.state.returnError;
+      return (
+        <div>
+          <div className="render-coontainer">
+            {" "}
+            {returnError ? (
+              <div className="search-error">
+                ERROR : {this.props.title}
+                did not return any results{" "}
+              </div>
+            ) : (
+              <div className="painting flx-ctr">
+                <div>
+                  <svg className="loading" viewBox="25 25 50 50">
+                    <circle cx="50" cy="50" r="20">
+                      {" "}
+                    </circle>{" "}
+                  </svg>{" "}
+                </div>{" "}
+              </div>
+            )}{" "}
+          </div>{" "}
+        </div>
+      );
+    }
+  }
+}
